435c0b57def44c27d2440915508a7d8d6badd4b3
d4nkali <133923419+d4nkali@users.noreply.github.com>
2024-02-09 12:27:26 -0300

    Renomenado arquivo e pequenas mudanças

--- 08_02_24/exercicio_1.py
+++ 08_02_24/exercicio_1.py
@@ -9,22 +9,22 @@
 
 # Guardando as informações da pesquisa
 
-pesq_fem = 6 # Quantidade de mulheres
-pesq_mas = 9 # Quantidade de homens
+pesq_fem = 0 # Quantidade de mulheres
+pesq_mas = 0 # Quantidade de homens
 # Registro das alturas
-pesq_alt = [1.70, 2.00, 1.76, 1.52, 1.74, 1.63, 1.87, 1.92, 1.71, 1.99, 2.03, 1.87, 1.59, 1.78, 1.88]
+pesq_alt = []
 # Registro das alturas dos homens
-pesq_alt_mas = [2.00, 1.76, 1.99, 2.03, 1.92, 1.88, 1.87, 1.70, 1.52]
+pesq_alt_mas = []
 
 # Calculos e comparativos
 
 pesq_alt_maior = max(pesq_alt) # Compara e armazena a maior altura da pesquisa
 pesq_alt_menor = min(pesq_alt)  # Compara e armazena a menor altura da pesquisa
-pesq_alt_mas_med = sum(pesq_alt_mas)/9  # Calcula a media de altura dos homens
+pesq_alt_mas_med = sum(pesq_alt_mas) / pesq_mas  # Calcula a media de altura dos homens
 
 # Exibindo os resultados das pesquisas
 
-print("Resultado da Pesquisa:")
+print("Resultado da Pesquisa:") # Começa imprimir os resultados
 print() # Espaçamento
 print("A maior altura foi: ", pesq_alt_maior, " e a minima de: ", pesq_alt_menor) # Imprime a maior e menor altura da pesquisa
 print() # Espaçamento
