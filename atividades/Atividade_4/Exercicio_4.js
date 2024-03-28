0d0df3c6cfd23bf93ed8a850180796ba8873c7e3
d4nkali <133923419+d4nkali@users.noreply.github.com>
2024-03-28 10:17:17 -0300

    Adicionando o segundo objeto

--- atividades/Atividade_4/Exercicio_4.js
+++ atividades/Atividade_4/Exercicio_4.js
@@ -34,5 +34,46 @@ console.log(aluno_1.nome);
 console.log(aluno_1.idade);
 console.log(aluno_1.filiacao[0]);
 console.log(aluno_1.filiacao[1]);
+console.log(aluno_1.endereco)
 console.log();
-console.log("Os dados estão preenchidos? " + aluno_1.dadosCompletos());
+console.log("Os dados estão preenchidos: " + aluno_1.dadosCompletos());
+
+var aluno_2 = {
+
+    nome: "Fabio",
+    idade: 21,
+    filiacao: ["Jose", "Marileide"],
+    endereco: {
+
+        rua: "",
+        cidade: "Patos",
+        estado: "PB",
+
+    },
+
+    dadosCompletos: function() {
+
+        if (this.nome && this.idade && this.filiacao[0] && this.filiacao[1] 
+            && this.endereco.rua && this.endereco.cidade && this.endereco.estado) {
+
+            return true;
+
+        }
+
+        else {
+
+            return false;
+
+        }
+
+    },
+
+}
+
+console.log(aluno_2.nome);
+console.log(aluno_2.idade);
+console.log(aluno_2.filiacao[0]);
+console.log(aluno_2.filiacao[1]);
+console.log(aluno_2.endereco)
+console.log();
+console.log("Os dados estão preenchidos: " + aluno_2.dadosCompletos());
