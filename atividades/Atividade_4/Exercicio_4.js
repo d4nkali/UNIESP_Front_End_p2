1630274cac699dc0d76f7401fc2bb8a70991adca
d4nkali <133923419+d4nkali@users.noreply.github.com>
2024-03-28 11:56:41 -0300

    Pequenos ajustes e comentarios

--- atividades/Atividade_4/Exercicio_4.js
+++ atividades/Atividade_4/Exercicio_4.js
@@ -1,9 +1,30 @@
-var aluno_1 = {
+/*
 
-    nome: "Roger",
-    idade: 26,
-    filiacao: ["Reginaldo", "Joana"],
-    endereco: {
+Exercicio 4 - Crie dois objetos com a estrutura abaixo:
+
+- Nome (tipo String)
+- Idade (tipo Number)
+- Filiacao (Array): nomeDoPai (tipo String) e nomeDaMae (tipo String)
+- Endereco (tipo Objeto): rua (tipo String), cidade (tipo String) e estado (tipo String)
+- dadosCompletos (tipo função) (verifica se os dados acimas estão preenchidos e retorna true, 
+caso positivo, e false, caso negativo)
+
+
+Crie uma função que receba os dois objetos acima criados, imprima os valores de suas propriedades
+e por fim imprima o resultado da função chamada dadosCompleto() que deve verificar se todos 
+os campos estão preenhidos.
+
+
+Aluno: Danilo Pereira Viana - P2-B
+
+*/
+
+var aluno_1 = { //* Criando objeto 'aluno_1'
+
+    nome: "Roger", // Tipo string
+    idade: 26, // Tipo number
+    filiacao: ["Reginaldo", "Joana"], // Array
+    endereco: { // Objeto
 
         rua: "Ferroviaria",
         cidade: "Campina Grande",
@@ -11,18 +32,19 @@ var aluno_1 = {
 
     },
 
-    dadosCompletos: function() {
+    dadosCompletos: function() { //* Criando a função 'dadosCompletos'
 
+        //! Se todos os campos de dados do objeto estiverem preenchidos, então:
         if (this.nome && this.idade && this.filiacao[0] && this.filiacao[1] 
-            && this.endereco.rua && this.endereco.cidade && this.endereco.estado) {
+        && this.endereco.rua && this.endereco.cidade && this.endereco.estado) {
 
-            return true;
+            return true; // Retorna o valor verdadeiro
 
         }
 
-        else {
+        else { //! Senão:
 
-            return false;
+            return false; // Retorna o valor falso
 
         }
 
@@ -30,12 +52,12 @@ var aluno_1 = {
 
 }
 
-var aluno_2 = {
+var aluno_2 = { //* Criando objeto 'aluno_2'
 
-    nome: "Fabio",
-    idade: 21,
-    filiacao: ["Jose", "Marileide"],
-    endereco: {
+    nome: "Fabio", // Tipo string
+    idade: 21, // Tipo number
+    filiacao: ["Jose", "Marileide"], // Array
+    endereco: { // Objeto
 
         rua: "",
         cidade: "Patos",
@@ -43,18 +65,19 @@ var aluno_2 = {
 
     },
 
-    dadosCompletos: function() {
+    dadosCompletos: function() { //* Criando a função 'dadosCompletos'
 
+        //! Se todos os campos de dados do objeto estiverem preenchidos, então:
         if (this.nome && this.idade && this.filiacao[0] && this.filiacao[1] 
-            && this.endereco.rua && this.endereco.cidade && this.endereco.estado) {
+        && this.endereco.rua && this.endereco.cidade && this.endereco.estado) {
 
-            return true;
+            return true; // Retorna o valor verdadeiro
 
         }
 
-        else {
+        else { //! Senão:
 
-            return false;
+            return false; // Retorna o valor falso
 
         }
 
@@ -62,33 +85,37 @@ var aluno_2 = {
 
 }
 
-function imprimirResultados() {
+function imprimirResultados() { //* Criando a função 'imprimirResultados'
 
     console.log("Imprimindo os resultados");
     console.log();
 
-    console.log("Aluno 1: ");
-    console.log();
-    console.log(aluno_1.nome);
-    console.log(aluno_1.idade);
-    console.log(aluno_1.filiacao[0]);
-    console.log(aluno_1.filiacao[1]);
-    console.log(aluno_1.endereco);
-    console.log();
-    console.log("Os dados estão preenchidos: " + aluno_1.dadosCompletos());
+    //! Imprime as informações de 'aluno_1'
+
+        console.log("Aluno 1: ");
+        console.log();
+        console.log("Nome: " + aluno_1.nome);
+        console.log("Idade: " + aluno_1.idade);
+        console.log("Nome do Pai: " + aluno_1.filiacao[0]);
+        console.log("Nome da Mãe: " + aluno_1.filiacao[1]);
+        console.log(aluno_1.endereco);
+        console.log();
+        console.log("Os dados estão preenchidos: " + aluno_1.dadosCompletos());
 
     console.log();
 
-    console.log("Aluno 2: ");
-    console.log();
-    console.log(aluno_2.nome);
-    console.log(aluno_2.idade);
-    console.log(aluno_2.filiacao[0]);
-    console.log(aluno_2.filiacao[1]);
-    console.log(aluno_2.endereco);
-    console.log();
-    console.log("Os dados estão preenchidos: " + aluno_2.dadosCompletos());
+    //! Imprime as informações de 'aluno_2'
+
+        console.log("Aluno 2: ");
+        console.log();
+        console.log("Nome: " + aluno_2.nome);
+        console.log("Idade: " + aluno_2.idade);
+        console.log("Nome do Pai: " + aluno_2.filiacao[0]);
+        console.log("Nome da Mãe: " + aluno_2.filiacao[1]);
+        console.log(aluno_2.endereco);
+        console.log();
+        console.log("Os dados estão preenchidos: " + aluno_2.dadosCompletos());
 
 }
 
-imprimirResultados();
+imprimirResultados(); //* Chama a função 'imprimirResultados'
